26b3c84b0a474dcc52e03ea7a564deefda707b06
filelucker <filelucker@gmail.com>
2023-04-17 14:29:21 +0600

    E/OpenCV: 1 ArUco marker(s) detected E/perimeteRatio: 16.501129167437256 cm E/distanceLength: 3.810026936996829 cm E/distanceGrith: 2.1261156969689177 cm I/System.out: 261 E/cowWeight: 261 kg

--- app/src/main/java/com/example/opencvexample/MainActivity.java
+++ app/src/main/java/com/example/opencvexample/MainActivity.java
@@ -15,6 +15,8 @@ import org.opencv.aruco.Aruco;
 import org.opencv.aruco.DetectorParameters;
 import org.opencv.aruco.Dictionary;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
@@ -27,13 +29,18 @@ public class MainActivity extends AppCompatActivity {
     Mat mat;
     Bitmap bitmap;
 
+    // Define the size of the marker in centimeters and the size of the marker in pixels
+    private static final double MARKER_SIZE_CM = 80.0;
+    private double mMarkerSizePixels;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         if (OpenCVLoader.initDebug()) {
-            Log.i("OpenCV", "Loader SUCCESS");
+            Log.e("OpenCV", "Loader SUCCESS");
         }
 
 
@@ -41,8 +48,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         // Load the bitmap image
-        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.aruco);
-//        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ranu);
+//        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.aruco);
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.niloy);
         imageView.setImageBitmap(bitmap);
 
         mat = new Mat();
@@ -50,11 +57,11 @@ public class MainActivity extends AppCompatActivity {
 
         Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY);
 
-        Utils.matToBitmap(mat, bitmap);
-        imageView.setImageBitmap(bitmap);
+//        Utils.matToBitmap(mat, bitmap);
+//        imageView.setImageBitmap(bitmap);
 
 
-        Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_6X6_250);
+        Dictionary dictionary = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_1000);
         DetectorParameters parameters = DetectorParameters.create();
 
         Mat ids = new Mat();
@@ -63,10 +70,55 @@ public class MainActivity extends AppCompatActivity {
 
         if (ids.total() > 0) {
             // ArUco marker(s) detected
-            Log.i("OpenCV", String.valueOf(ids.total()) + " ArUco marker(s) detected");
+            Log.e("OpenCV", String.valueOf(ids.total()) + " ArUco marker(s) detected");
+
+            // Get the corner points of the marker
+            List<Point> markerCorners = new ArrayList<Point>();
+            for (int i = 0; i < corners.size(); i++) {
+                markerCorners.add(new Point(corners.get(i).get(0, 0)));
+                markerCorners.add(new Point(corners.get(i).get(1, 0)));
+                markerCorners.add(new Point(corners.get(i).get(2, 0)));
+                markerCorners.add(new Point(corners.get(i).get(3, 0)));
+            }
+
+            // Calculate the perimeter of the marker in pixels
+            mMarkerSizePixels = Imgproc.arcLength(new MatOfPoint2f(markerCorners.toArray(new Point[markerCorners.size()])), true);
+
+
+//            E/distancea2b:: 640.3381918955014 pixel
+//            E/distancec2d:: 357.33037934102384 pixel
+//            E/scaleFactor: 0.09625668
+//            E/areaOnCanvas: 874800.0
+//            E/totalCanvasArea: 2088720.0
+//            E/scaleFactor: 0.09818182
+
+
+
+            // Calculate the perimeter ratio of the marker in centimeters
+            double perimeteRatio = (mMarkerSizePixels * 0.09818182) / MARKER_SIZE_CM;
+            Log.e("perimeteRatio ", String.valueOf(perimeteRatio) + " cm");
+
+            double distanceLength = (640.34 * 0.09818182)  / perimeteRatio;
+            Log.e("distanceLength ", String.valueOf(distanceLength) + " cm");
+
+            double distanceGrith = (357.33 * 0.09818182)  / perimeteRatio;
+            Log.e("distanceGrith ", String.valueOf(distanceGrith) + " cm");
+
+            double cowWeight = (distanceLength * distanceGrith * distanceGrith) / 660;
+
+            int intValue = (int) Math.round(cowWeight * 10000);
+            Log.e("cowWeight ", String.valueOf(intValue) + " kg");
+
+
+
+
+
+
+
+
         } else {
             // No ArUco marker detected
-            Log.i("OpenCV", "No ArUco marker detected");
+            Log.e("OpenCV", "No ArUco marker detected");
         }
 
 
